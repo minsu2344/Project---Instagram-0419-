7454d7e57a0f7ca8fe3397a14da68d3730073482
minsoo <minsu2344@naver.com>
2022-04-19 11:45:15 +0900

    상단 네이게이션바

---
+++ src/modules/nav/Nav.js
@@ -0,0 +1,22 @@
+//1.내비게이션 영역인 Nav bar 기본 구조를 생성하세요.
+const Nav = () => `
+  <nav>
+    <a href='#' class='logo'>
+      <img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' alt='Logo'>
+    </a>
+    <input type="text" class="search" placeholder="Search">
+    <span class="nav-links">
+      <a href="#"><i class="fas fa-home"></i></a>
+      <a href="#"><i class="far fa-plus-square"></i></a>
+      <a href="#"><i class="far fa-compass"></i></a>
+      <a href="#"><i class="far fa-heart"></i></a>
+    </span>
+  </nav>
+`
+
+export default Nav;
+
+//2. `<nav>...</nav> `내에 내비게이션 바의 총 3가지 요소들(인스타그램 로고, 검색창, 상단 메뉴 탭)을 구성하세요.
+
+
+
